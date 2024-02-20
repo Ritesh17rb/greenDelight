1383e62c0b8a3add5ff8b4acf1ec3ba14565454c
Ritesh17rb <ritesh17lifeamazing@gmail.com>
2024-02-20 23:56:56 +0530

    Before making backend connection using axios

---
+++ client/src/pages/Auth/Register.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import Layout from '../../components/Layout/Layout';
+
+const Register = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phone, setPhone] = useState("");
+    const [address, setAddress] = useState("");
+
+    // Form function
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(name, email, password, address, phone)
+    }
+
+    return (
+        <Layout title={'Register Yourself'}>
+            <div className="register">
+                <h1>Register Page</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputName">Name <span style={{ color: 'red' }}>*</span></label>
+                        <input type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            className="form-control"
+                            id="exampleInputName"
+                            placeholder="Enter Name"
+                            required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputEmail">Email <span style={{ color: 'red' }}>*</span></label>
+                        <input type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="form-control"
+                            id="exampleInputEmail"
+                            placeholder="Enter Email"
+                            required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputPassword1">Password <span style={{ color: 'red' }}>*</span></label>
+                        <input type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="form-control"
+                            id="exampleInputPassword1"
+                            placeholder="Password"
+                            required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputPhone">Phone <span style={{ color: 'red' }}>*</span></label>
+                        <input type="text"
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                            className="form-control"
+                            id="exampleInputPhone"
+                            placeholder="Enter Phone"
+                            required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputAddress">Address <span style={{ color: 'red' }}>*</span></label>
+                        <input type="text"
+                            value={address}
+                            onChange={(e) => setAddress(e.target.value)}
+                            className="form-control"
+                            id="exampleInputAddress"
+                            placeholder="Enter Address"
+                            required />
+                    </div>
+                    <button type="submit" className="btn btn-info my-4">Submit</button>
+                </form>
+            </div>
+        </Layout>
+    );
+}
+
+export default Register;
---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+
+const HomePage = () => {
+    return (
+        <Layout title={'Green Delight- Best Offers Buy now'}>
+            <h1>HomePage</h1>
+        </Layout >
+    );
+};
+
+export default HomePage;
