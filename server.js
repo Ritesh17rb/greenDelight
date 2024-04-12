9ca86687da1b1614fcad001be81e113645eeff9a
Ritesh17rb <ritesh17lifeamazing@gmail.com>
2024-04-12 09:21:32 +0530

    Fiexed issue in File Path Import

--- server.js
+++ server.js
@@ -1,51 +1,48 @@
 import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
-import morgan from "morgan"
+import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from './routes/authRoute.js';
-import categoryRoutes from "./routes/categoryRoutes.js"
-import productRoutes from "./routes/productRoutes.js"
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 import cors from 'cors';
-import path from 'path'
+import path from 'path';
 import { fileURLToPath } from 'url';
 
 // Configure env
-dotenv.config()
+dotenv.config();
 
 // database config
 connectDB();
 
 // Esmodule
-const __filename = fileURLToPath(import.meta.url)
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Rest object
-const app = express()
+const app = express();
 
 // middlewares
-app.use(cors())
-app.use(express.json())
-app.use(morgan('dev'))
-
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
 
 // routes
 app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/category', categoryRoutes)
-app.use('/api/v1/product/', productRoutes)
-app.use(express.static(path.join(__dirname, './client/build')))
+app.use('/api/v1/category', categoryRoutes);
+app.use('/api/v1/product/', productRoutes);
+app.use(express.static(path.join(__dirname, './client/build')));
 
 // Rest APIs
 app.use('*', function (req, res) {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
-    res.sendFile(path.join(__dirname, './client/build/index.html'))
-
-})
 // PORT
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 // Run Listen
-
 app.listen(PORT, () => {
-    console.log(`Server Running on PORT : ${PORT}`.bgCyan.white)
-})
+    console.log(`Server Running on PORT : ${PORT}`.bgCyan.white);
+});
